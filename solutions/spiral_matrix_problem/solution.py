5dd638773859c88e4f13c121e080570b7d149613
Telnov Artem <telnowtema@mail.ru>
2023-03-10 00:01:17 +0300

    fix naming in spiral_matrix (#8)

--- solutions/spiral_matrix_problem/solution.py
+++ solutions/spiral_matrix_problem/solution.py
@@ -52,41 +52,41 @@ class Solution(object):
         # считаем плозадь всего прямоугольника(кол-во всех элементов в матрице)
         count_all_numbers = n * m
 
-        def filling_layer(lvl=0):
+        def filling_layer(layer=0):
             """
             Ф-циф, которая будет заполнять матрицу по слоям
             """
             # считаем какое кол-во колонок и строк на данном слое
-            column_count= m - lvl * 2
-            row_count= n - lvl * 2
+            column_count= m - layer * 2
+            row_count= n - layer * 2
             # находим число с которого начинается данный слой
             current_count_numbers = row_count * column_count
             current_num = count_all_numbers - current_count_numbers + 1
 
             # заполняем всю верхушку, кроме последнего символа
             for i in range(column_count-1):
-                res[lvl][lvl + i] = current_num
+                res[layer][layer + i] = current_num
                 current_num += 1
 
             # заполняем весь правый ряд, кроме последнего символа
             for i in range(row_count-1):
-                res[lvl + i][lvl + column_count - 1] = current_num
+                res[layer + i][layer + column_count - 1] = current_num
                 current_num += 1
 
             # заполняем низ, кроме последнего
             for i in range(column_count-1):
-                res[lvl + row_count - 1][lvl + column_count - 1 - i] = current_num
+                res[layer + row_count - 1][layer + column_count - 1 - i] = current_num
                 current_num += 1
 
             # заполняем левый край, кроме последнего. Он у нас уже заполнен и равен начальному.
             for i in range(row_count-1):
-                res[lvl + row_count - 1 - i][lvl] = current_num
+                res[layer + row_count - 1 - i][layer] = current_num
                 current_num += 1
             
             # Если текущий размер матрицы больше чем 2 по обеим из сторон, то мы еще не закончили.
             if column_count > 2 and row_count > 2:
                 # увеличиваем слой и спускаемся ниже
-                filling_layer(lvl + 1)
+                filling_layer(layer + 1)
 
         filling_layer()
 
