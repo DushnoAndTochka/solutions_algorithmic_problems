6fcfb154192d9056d471fa1e7a289d58868f00c6
Telnov Artem <telnowtema@mail.ru>
2023-03-10 21:34:57 +0300

    add MergeTwoLinkedSortedLists problem and solution (#10)

---
+++ solutions/merge_two_sorted_lists/solution/solution.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+
+class ListNode(object):
+    """Узел односвязанного списка."""
+
+    def __init__(self, val: int = 0, next: ListNode = None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    
+    def merge_two_sorted_lists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        # создаем рузельтирующую голову
+        head = ListNode()
+        # Переменная указывающая на последнбб отсортированнуб ноду.
+        current_node = head
+
+        # цикл, который работает сразу с двумя списками
+        while list1 is not None and list2 is not None:
+            
+            if list2.val > list1.val:
+                # Если значение в первом меньше чем в втором,
+                # значит мы к результату прилинковываем эту нод
+                # и переходим к следующей.
+                current_node.next = list1
+                list1 = list1.next
+            else:
+                # Обратное к предыдущему.
+                current_node.next = list2
+                list2 = list2.next
+
+            # Так как мы отсортировали уже следующую ноду, мы переходим к ней.
+            current_node = current_node.next
+
+        # цикл досортировки первого списка
+        while list1 is not None:
+            current_node.next = list1
+            list1 = list1.next
+            current_node = current_node.next
+
+        # цикл досортировки второго списка
+        while list2 is not None:
+            current_node.next = list2
+            list2 = list2.next
+            current_node = current_node.next
+
+        # не забываем, что HEAD указывает на пустышку. 
+        # нас интересует следующая за ним нода.
+        return head.next
