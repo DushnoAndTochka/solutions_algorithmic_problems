eeef0a42d34209f9a4ab3256051db1dddd516e76
Telnov Artem <telnowtema@mail.ru>
2023-03-13 13:21:19 +0300

    Add length-of-last-word- solution. (#14)

--- solutions/length_of_last_word/solution/solution.py
+++ solutions/length_of_last_word/solution/solution.py
@@ -1,4 +1,14 @@
 class Solution(object):
 
     def length_of_last_word(self, s: str) -> int:
-        pass
+        counter = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            if counter == 0 and s[i] == ' ':
+                continue
+            elif counter != 0 and s[i] == ' ':
+                return counter
+            elif s[i] != ' ':
+                counter += 1
+
+        return counter
