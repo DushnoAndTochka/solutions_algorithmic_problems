7c1a91514bf5baef594323ca8a48c81b99996260
Telnov Artem <telnowtema@mail.ru>
2023-03-14 23:18:59 +0300

    fix length_of_last_world solution (#20)

--- solutions/length_of_last_word/solution/solution.py
+++ solutions/length_of_last_word/solution/solution.py
@@ -1,14 +1,18 @@
 class Solution(object):
 
     def length_of_last_word(self, s: str) -> int:
-        counter = 0
-
+        len_last_word = 0
         for i in range(len(s) - 1, -1, -1):
-            if counter == 0 and s[i] == ' ':
-                continue
-            elif counter != 0 and s[i] == ' ':
-                return counter
-            elif s[i] != ' ':
-                counter += 1
+            if s[i] == " ":
+                if len_last_word == 0:
+                    # Если наш счетчик равен нулю, 
+                    # значит мы еще не встречали буквы и 
+                    # эти проблемы для нас не показательны.
+                    continue
+                # Если наш счетчик не равен нулю, 
+                # значит мы уже встречали буквы и 
+                # прошли все слово целиком.
+                return len_last_word
+            len_last_word += 1
 
-        return counter
+        return len_last_word
