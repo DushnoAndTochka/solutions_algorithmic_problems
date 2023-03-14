d91715e286606cb3f9eb33c5496554699b93b5c8
Telnov Artem <telnowtema@mail.ru>
2023-03-14 17:56:30 +0300

    Add two numbers solution (#18)
    * add add_two_numbers solution
    
    * add add_two_numbers bonus solution

---
+++ solutions/add_two_numbers/bonus_solution/solution.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val: int=0, next: Optional[ListNode]=None):
+        self.val = val
+        self.next = next
+        
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Считаем что первый список является результируеющем. 
+        # Выставляем на него все необходимые указатели.
+        # result - то что улетит из ф-ции как ответ
+        # current_result_node - нода которую мы модифицировали последней
+        result = current_result_node = l1
+        # делаем себе якорь на случай, если окажется 
+        # что l2 более длинный список.
+        l2_result = l2
+
+        member_num = 0
+
+        while l1 is not None or l2 is not None:
+            next_val = member_num
+
+            if l1 is not None:
+                next_val += l1.val
+
+            if l2 is not None:
+                next_val += l2.val
+            
+            if next_val >= 10:
+                member_num = 1
+                next_val -= 10
+            else:
+                member_num = 0
+            # До этих пор все шло как и предыдущем решении. 
+            # Только мы l1 и l2 не переключали дальше.
+
+            if l1 is None:
+                # Если у нас закончились ноды в l1 списке,
+                # значит мы будем работать с l2 и 
+                # результат переключим на l2, 
+                # так как он оказался более длинным
+                current_result_node = l2
+                result = l2_result
+                l2.val = next_val
+                l2 = l2.next
+            elif l2 is None:
+                # ровно наоборот, только якорь нам не нужен,
+                # получается что мы и так угадали с выбором.
+                current_result_node = l1
+                l1.val = next_val
+                l1 = l1.next
+            else:
+                # если оба списка еще не закончились, то работаем с ними.
+                current_result_node = l1
+                l2.val = next_val
+                l2 = l2.next
+                l1.val = next_val
+                l1 = l1.next
+                
+                
+
+        if member_num:
+            current_result_node.next = ListNode(val=1)
+
+        return result
---
+++ solutions/add_two_numbers/solution/solution.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val: int=0, next: Optional[ListNode]=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    def add_two_numbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # создаем голову результирующего списка
+        result_head = result = ListNode()
+        # переменная для "держать в уме"
+        member_num = 0
+
+        # пока хотя бы одна нода не закончена, мы идем
+        while l1 is not None or l2 is not None:
+            # переменная в которой будет результат суммы двух нод,
+            # с учетом числа, что мы запомнил с прошлой операции.
+            next_val = member_num
+
+            if l1 is not None:
+                # если l1 доступна, берем значение и 
+                # двигаемся по листу дальше
+                next_val += l1.val
+                l1 = l1.next
+
+            if l2 is not None:
+                # если l2 доступен, берем значение и 
+                # двигаемся по листу дальше
+                next_val += l2.val
+                l2 = l2.next
+            
+            # если мы насчитали больше 10, 
+            # значит единицу надо перенести в следующий разряд
+            if next_val >= 10:
+                member_num = 1
+                next_val -= 10
+            else:
+                member_num = 0
+            
+            # складываем то что насчитали и создаем следующую 
+            # ноду в результирующем линкед листе
+            result.val = next_val
+            result.next = ListNode()
+            result = result.next
+
+        if member_num:
+            # может получиться так, что два числа в сумме дают число,
+            # которое больше чем они по разряду, не стоит этого забывать.
+            # Пример: 9 + 1 = 10
+            result.next = ListNode(val=1)
+
+        return result_head
