ff20e44a2ac0539abb860f8d2b9d4c8f2e4e348c
Telnov Artem <telnowtema@mail.ru>
2023-03-16 10:43:27 +0300

    add linked-list-cycle problem (#23)

---
+++ solutions/linked_list_cycle/solution/solution.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x: int):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        pass
