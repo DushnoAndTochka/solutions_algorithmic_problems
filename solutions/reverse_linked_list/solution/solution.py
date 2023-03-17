a13ce4371c8e315ab2904485bb2a39047ce2eb8c
Andrei Berenda <berenda.andrey@gmail.com>
2023-03-17 16:15:10 +0000

    Add solution for reverse lined list (#26)

--- solutions/reverse_linked_list/solution/solution.py
+++ solutions/reverse_linked_list/solution/solution.py
@@ -1,13 +1,20 @@
-from __future__ import annotations
 from typing import Optional
 
 
 class ListNode:
-    def __init__(self, val: int=0, next: Optional[ListNode]=None):
+    def __init__(self, val: int = 0, next: Optional['ListNode'] = None):
         self.val = val
         self.next = next
 
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        prev = None
+        curr = head
+
+        while curr is not None:
+            next_ = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_
+        return prev
