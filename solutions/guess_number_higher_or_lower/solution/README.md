ddb82839d80416f472595d78061d7c3b82a86161
Telnov Artem <telnowtema@mail.ru>
2023-03-22 20:30:26 +0300

    This is a new commit for what I originally planned to be amended (#35)

--- solutions/guess_number_higher_or_lower/solution/README.md
+++ solutions/guess_number_higher_or_lower/solution/README.md
@@ -1,5 +1,7 @@
 # GuessNumberHigherOrLower Solution
 
-Описание будет позже =) 
+## Описание решения.
+Решение в точности повторяет задачу bynary_search, за одним лишь исключением. Вместо того что бы самим сравнивать число с таргетом, мы отпрвляем запрос в ф-цию, которая дулает это за нас.
 
-Всем хорошего дня!
+---
+[Идею решения бинарного поиска, можно посмотреть тут.](https://github.com/artem-telnov/solutions_algorithmic_problems/tree/main/solutions/binary_search/solution)
--- solutions/guess_number_higher_or_lower/solution/solution.py
+++ solutions/guess_number_higher_or_lower/solution/solution.py
@@ -9,7 +9,23 @@ def guess(num: int) -> int:
         return 0
     
 
+
 class Solution:
-    def guessNumber(self, n: int) -> int:
-        pass
-    
+    def guessNumber(self, n:int) -> int:
+        # Как и в классическое задаче по бинпоиску,
+        # выставляем границы
+        left, rigth = 0, n
+
+        while left <= rigth:
+            mid = (left + rigth) // 2
+
+            # берем середину и делаем предположение.
+            current_guess = guess(mid)
+
+            # if-ы один к одному как в условии задачи.
+            if current_guess == 0:
+                return mid
+            elif current_guess == 1:
+                left = mid + 1
+            else:
+                rigth = mid - 1
