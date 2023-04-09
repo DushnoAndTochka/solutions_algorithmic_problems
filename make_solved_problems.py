2aa441e762b012875efa86528c5ae5dbd29c594b
artem-telnov <telnowtema@mail.ru>
2023-04-09 14:13:41 +0300

    Change make_solved_skript; Add github action workflow;

--- make_solved_problems.py
+++ make_solved_problems.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python3
+import csv
 import pathlib
 import re
 
 REGEX = re.compile('^<a href="(https://leetcode.com/problems/.+/)">.+')
+CSV_HEADERS = ["problem_name", "problem_origin_url", "is_solved_problem"]
 
 
-def get_solved_tasks(parent: pathlib.Path):
+def get_solved_tasks(parent: pathlib.Path) -> set[tuple[str, str]]:
     solutions = parent / "solutions"
     solved = set()
     for solution in solutions.iterdir():
-        readme = solution / "README.md"
-        with open(readme) as f:
+        problem_name = str(solution).split('/')[1]
+        problem_readme = solution / "README.md"
+        with open(problem_readme) as f:
             line = f.readlines()[-1]
             m = REGEX.match(line)
-            assert REGEX.match(line), readme
-            problem = m.group(1) + "\n"
+            assert m, problem_readme
+            problem = m.group(1)
             assert problem not in solved, problem
-            solved.add(problem)
+
+        solution_path = solution / 'solution'
+        if (solution_path in solution.iterdir()
+                and solution_path / 'README.md' in solution_path.iterdir()):
+            solved.add((problem_name, problem, True))
+        else:
+            solved.add((problem_name, problem, False))
     return sorted(solved)
+
+
+def write_result(parent: pathlib.Path, solved: set[tuple[str, str]]) -> None:
+    with open(parent / "solved_problems.csv", "wt") as f:
+        writer = csv.writer(f, delimiter=",")
+        writer.writerow(CSV_HEADERS)
+        writer.writerows(solved)
 
 
 def main():
     parent = pathlib.Path().parent
-    with open(parent / "solved_problems.txt", "w") as f:
-        f.writelines(get_solved_tasks(parent))
+    solved_problems = get_solved_tasks(parent)
+    write_result(parent, solved_problems)
 
 
 if __name__ == "__main__":
