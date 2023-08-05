675bc0616bfb4769962c5f073f033f09dc1ec2d6
Telnov Artem <telnowtema@mail.ru>
2023-08-05 13:43:33 +0200

    Single Element in a Sorted Array Solution FIX (#132)

--- solutions/single_element_in_a_sorted_array/solution/solution.py
+++ solutions/single_element_in_a_sorted_array/solution/solution.py
@@ -1,8 +1,26 @@
 class Solution:
     def single_non_duplicate(self, nums: list[int]) -> int:
-        result = 0
+        n = len(nums)
 
-        for i in nums:
-            result ^= i
+        if n == 1 or nums[0] != nums[1]:
+            return nums[0]
+        if nums[-2] != nums[-1]:
+            return nums[-1]
 
-        return result
+        lp = 1
+        rp = n - 2
+
+        while lp <= rp:
+            mp = (lp + rp) // 2
+
+            if nums[mp] != nums[mp - 1] and nums[mp] != nums[mp + 1]:
+                return nums[mp]
+            if (
+                mp % 2 == 1
+                and nums[mp] == nums[mp - 1]
+                or mp % 2 == 0
+                and nums[mp] == nums[mp + 1]
+            ):
+                lp = mp + 1
+            else:
+                rp = mp - 1
